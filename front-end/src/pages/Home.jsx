2371d64721aac706edd51c76fb76b03880c54da8
HanoodTunio <hanood.tunio@gmail.com>
2025-03-19 05:59:43 +0500

    Skills Certification card added

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../components/Layout/Navbar";
 import Banner from "../components/specific/Banner";
 import Footer from "../components/Layout/Footer";
+import SkillsCareerSection from "../components/specific/SkillsCareerSection";
 
 function Home() {
   return (
     <>
       <Navbar />
       <Banner />
+      <SkillsCareerSection />
       <Footer />
     </>
   );
